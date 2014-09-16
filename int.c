ee5aee08c452a18368cfb880a814d90888a06623
LastAvenger <LastAvengers@outlook.com>
2014-09-16 21:53:05 +0800

    更新到 day 7, harib04g
    支持鼠标中断.

--- int.c
+++ int.c
@@ -40,15 +40,18 @@ void inthandler21(int *esp)
 	return;
 }
 
+
+struct FIFO8 mousefifo;
+
 void inthandler2c(int *esp)
-/*来自PS/2鼠标的中断 */
+/*来自PS/2 鼠标的中断 */
 {
-	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
-	for (;;) {
-		io_hlt();
-	}
+	unsigned char data;
+	io_out8(PIC1_OCW2, 0x64);	/* 通知PIC IRQ-12 的受理已经完成*/
+	io_out8(PIC0_OCW2, 0x62);	/* 通知PIC IRQ-02 的受理已经完成*/
+	data = io_in8(PORT_KEYDAT);
+	fifo8_put(&mousefifo, data);
+	return;
 }
 
 // 特殊机种的处理
